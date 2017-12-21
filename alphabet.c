328a0110363d460cae04394d1350691b082f98e6
logavignesh123 <34537788+logavignesh123@users.noreply.github.com>
2017-12-21 17:01:04 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+    if((ch>=97&&ch<=122) || (ch>=65&&ch<=90))
+    {
+        printf("Character is an ALPHABET.");
+    }
+    else
+    {
+        printf("Character is NOT ALPHABET.");
+    }
+    return 0;
+}
