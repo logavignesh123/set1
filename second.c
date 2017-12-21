ba8ce5b4a138f8d2a11ce3c03ad0f9055131e5a4
logavignesh123 <34537788+logavignesh123@users.noreply.github.com>
2017-12-21 10:23:42 +0530

    Create second.c

---
+++ second.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("\n enter a number");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("\n the given number is even");
+    }
+    else
+    printf("\n the given number is odd");
+}
